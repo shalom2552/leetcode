c5b8b39e26e0f46c547e3fb4f3c84c0c08739352
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-02-11 10:53:06 +0200

    Time: 1 ms (59.36%), Space: 20.7 MB (5.34%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        hashmap = {}
+
+        for i in range(len(nums)):
+            compliment = target - nums[i]
+
+            if compliment in hashmap.keys():
+                return [hashmap[compliment], i]
+
+            hashmap[nums[i]] = i
+        pass
