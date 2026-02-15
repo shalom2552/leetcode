71dd6a41c256d056e7ea7ceae039cebea91236ef
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-02-15 16:55:06 +0200

    Time: 0 ms (100%), Space: 20.7 MB (5.83%) - Easy
    Time: 0 ms (100%), Space: 20.7 MB (5.83%) - Easy

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,13 +1,25 @@
+# Motivation: num1 + num2 = target => num1 = target - num2
+#       Use a hashmap to store the diff between each number and the target.
+#       Loop the array and in each iteration chek if the current number - target
+#       was seen before.
+# Complexity: 
+#     Time - O(n): Looping over the array once
+#     Space - O(n): Store each element once in worst case
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
+        # hashmap to store values and their index
         hashmap = {}
 
         for i in range(len(nums)):
+
+            # compliment is the second number to sum to target
             compliment = target - nums[i]
 
+            # is this number has seen before
             if compliment in hashmap.keys():
                 return [hashmap[compliment], i]
 
+            # add current number to the hashmap
             hashmap[nums[i]] = i
         pass
