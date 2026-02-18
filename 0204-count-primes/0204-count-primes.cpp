d7c6fddad08816912071b1088b89cba5870b35ee
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-02-18 20:36:18 +0200

    Time: 247 ms (14.32%), Space: 12.6 MB (65.27%) - Medium

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,42 @@
+/*
+Motivation: We'll initialize an array represents all the number from
+    0 to n-1 with boolean type sets to True.
+    Then we'll calculate all the non prime numbers using all the previous
+    prime numbers from the array, by multiplying them by the current value. 
+    That is all the numbers that have a devisor smaller than them are not prime.
+Complexity: 
+    Time - O(n*loglogn): n time p/n times for each p prime.
+        Sum of reciprocals of primes grows as loglogn:
+        https://en.wikipedia.org/wiki/Mertens'_theorems
+    Space - O(n): bool vector for all numbers up to n
+*/
+class Solution {
+public:
+    int countPrimes(int n) {
+        
+        // index representation of numbers up to n
+        std::vector<bool> is_prime(n, true);
+
+        // update is_prime with all non prime numbers
+        for (long i = 2; i < n; ++i) {
+            // if i is not prime it will not find a new non prime
+            if (is_prime[i]) {
+                // find all numbers that are divisble by i
+                for (long j = i * i; j < n; j += i) {
+                    // j is a multiple of smaller number diffrent than 1
+                    is_prime[j] = false;
+                }
+            }
+        }
+
+        // count all primes from 1 to n-1
+        int count = 0;
+        for (int num = 2; num < n; ++num) {
+            if (is_prime[num]) {
+                ++count;
+            }
+        }
+
+        return count;
+    }
+};
