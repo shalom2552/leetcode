ad80b246420adaf22290211ed907b80cefe4f167
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-02-22 15:16:39 +0200

    Time: 0 ms (100%), Space: 20.4 MB (35.18%) - Medium

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,22 +1,23 @@
-# Motivation: 
+# Motivation: Buy and sell every day, if it worth it.
+#     For every day, if the price is higher than yesterday, then we could have
+#     bought it and sell today. other wise we won't buy.
 # Complexity: 
 #     Time - O(n): Looping over the array once.
 #     Space - O(1): Auxiliary.
 #
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
-        # Can't profit from one day or less
-        if len(prices) < 1:
-            return 0
-
+        
         profit = 0
+        length = len(prices)
 
         # calculate profit from each day
-        for i in range(1, len(prices)):
+        for i in range(1, length):
+            today = prices[i]
+            yesterday = prices[i - 1]
 
-            # today's price is lower than yesterday's
-            if prices[i - 1] < prices[i]:
-                profit += prices[i] - prices[i - 1]
+            # yesterday's price is lower than today's
+            if yesterday < today:
+                profit += today - yesterday
 
         return profit
