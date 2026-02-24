fa9017ddcbedf17e65b7f12e7f762ead9d23f78f
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-02-24 19:40:55 +0200

    Time: N/A (0%), Space: N/A (0%) - Medium

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,4 +1,50 @@
+# Motivation: 2 for loops and a binary search on the third value.
+# Complexity: 
+#       Time - O(n^2*logn): sorting and nested binary search in nested for loops.
+#       Space - O(1): Auxiliary.
 class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        # sort the array
+        nums.sort()
+        results = []
+        
+        for i in range(len(nums)):
+
+            # skip duplicates
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+
+            # for each element after i
+            for j in range(i + 1, len(nums) - 1):
+
+                # skip duplicates
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = len(nums) - 1
+                
+                # binary search on third number
+                while (left <= right):
+
+                    mid = left + (right - left) // 2
+                    total = nums[i] + nums[j] + nums[mid]
+
+                    # update the search spcae or break if found
+                    if total == 0:
+                        results.append([nums[i], nums[j], nums[mid]])
+                        break
+                    elif (total < 0):
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+        
+        return results
+
+
+# Two pointers solution
+class SolutionPointers:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
         # sort the array
