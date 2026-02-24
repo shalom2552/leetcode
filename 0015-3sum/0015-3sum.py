36fdbee77ecfc25a5ad8285948d4adad3667114c
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-02-24 19:27:42 +0200

    Time: 660 ms (34.28%), Space: 22.2 MB (64.17%) - Medium

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,57 +1,43 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # Solution with 2 pointers, list to store results.
-        # and updating indexes to  avoid duplicates.
+
+        # sort the array
         nums.sort()
         results = []
         
         for i in range(len(nums)):
-            j, k = i + 1, len(nums) - 1
+            
+            # search space
+            j = i + 1
+            k = len(nums) - 1
+
+            # skip duplicate consecutive numbers
             if i > 0 and nums[i] == nums[i-1]:
                 continue
 
+            # binary search
             while(j < k):
+
                 total = nums[i] + nums[j] + nums[k]
+
                 if total == 0:
                     results.append([nums[i], nums[j], nums[k]])
+                    
+                    # keep searching with diffrent values 
                     j += 1
                     k -= 1
+
+                    # find the next diffrent values from both ends
                     while nums[j] == nums[j-1] and j < k:
                         j += 1
                     while nums[k] == nums[k+1] and j < k:
                         k -= 1
+
+                # increase from left
                 elif total < 0:
                     j += 1
-                    # while nums[j] == nums[j-1] and j < k:
-                    #     j += 1
+                # decrease from right
                 else:
                     k -= 1
-                    # while nums[k] == nums[k+1] and j < k:
-                    #     k -= 1
             
         return results
-
-
-# class Solution:
-#     def threeSum(self, nums: List[int]) -> List[List[int]]:
-#         # Solution with 2 pointers, and set to store results.
-#         # store results after sorting to avoid duplicates.
-#         # this solution go over all posibilities in the scope and does not skip.
-#         nums.sort()
-#         results = set()
-        
-#         for i in range(len(nums)):
-
-#             j, k = i + 1, len(nums) - 1
-
-#             while(j < k):
-#                 if nums[i] + nums[j] + nums[k] == 0:
-#                     results.add(tuple(sorted([nums[i], nums[j], nums[k]])))
-#                     j += 1
-#                     k -= 1
-#                 elif nums[i] + nums[j] + nums[k] < 0:
-#                     j += 1
-#                 else:
-#                     k -= 1
-
-#         return results
