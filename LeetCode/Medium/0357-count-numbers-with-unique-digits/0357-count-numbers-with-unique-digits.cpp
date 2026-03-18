cba8a62c1df86da0b4de7a7b7805e8eaa7230d78
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-03-18 10:11:40 +0200

    solve: 0357-count-numbers-with-unique-digits (.cpp)

--- LeetCode/Medium/0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
+++ LeetCode/Medium/0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.cpp
@@ -1,3 +1,14 @@
+/*
+* Motivation: Math / Combinatorics.
+*     Loop over the number lengths from 0 to n.
+*     For each length, calculate how many unique digit numbers we can make.
+*     The first digit has 9 choices (can't be 0), the second has 9 choices, the third has 8, and so on.
+*     Multiply these choices together in the inner loop and add them to the total sum.
+* Complexity: 
+*     Time - O(n): Looping up to n times (and n can be at most 10 before we run out of unique digits).
+*     Space - O(1): We only save the total and sub_total variables.
+*/
+
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
