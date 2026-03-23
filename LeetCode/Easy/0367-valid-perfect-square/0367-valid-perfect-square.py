3f2555f407f182e0d2a3199417f8f738125f9bec
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-03-23 13:38:45 +0200

    solve: 0367-valid-perfect-square (.py)

---
+++ LeetCode/Easy/0367-valid-perfect-square/0367-valid-perfect-square.py
@@ -0,0 +1,35 @@
+"""
+Motivation: Binary Search.
+    We know that sqrt(x) < x/2 for all x > 4
+    Therefore we can do a binary search on the search space (4, num/2]
+Complexity:
+    Time - O(log(num)): Binary Search up to n/2
+    Space - O(1): Auxiliary
+"""
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+
+        # we search from 4 so we must check 1 and 4 first
+        if (num == 1 or num == 4):
+            return True
+
+        # sqrt(x) < x/2 for all x > 4
+        left = 1            # 1
+        right = num // 2    # 4
+
+        # binary search on the search space
+        while left <= right:
+
+            mid = left + (right - left) // 2
+            sqr = mid * mid
+
+            # found integer sqrt(num)
+            if sqr == num:
+                return True
+
+            if sqr < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False
