6449d2167a0a04a78d2e82707ab44302beba1ec1
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-04-15 21:20:06 +0300

    refactor: delete duplicated 1757 .sql.py file

--- LeetCode/Easy/1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def find_products(products: pd.DataFrame) -> pd.DataFrame:
-    low_fat = products[products["low_fats"] == "Y"]
-    low_rec = low_fat[low_fat["recyclable"] == "Y"]
-    return low_rec[["product_id"]]
