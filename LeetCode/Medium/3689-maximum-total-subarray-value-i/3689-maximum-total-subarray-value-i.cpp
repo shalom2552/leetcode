015178fa39d7639170e377b73590eb0e2750e7d3
Shalom <64192029+shalom2552@users.noreply.github.com>
2026-06-09 12:13:34 +0300

    solve: 3689-maximum-total-subarray-value-i  (.cpp)

--- LeetCode/Medium/3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
+++ LeetCode/Medium/3689-maximum-total-subarray-value-i/3689-maximum-total-subarray-value-i.cpp
@@ -10,7 +10,6 @@ public:
     long long maxTotalValue(vector<int>& nums, int k) {
         int max = *std::max_element(nums.begin(), nums.end());
         int min = *std::min_element(nums.begin(), nums.end());
-
         return k * static_cast<long long>(max - min);
     }
 };
